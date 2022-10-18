c23b98e8f0b6aaca8c3b898a3e10701f1bf07095
caioalmeida08 <araujo.caio08@aluno.ifce.edu.br>
2022-10-18 12:35:55 -0300

    adicionei a conexao com o banco de dados da maneira correta

--- Main.java
+++ Main.java
@@ -1,25 +1,26 @@
 
-import com.mysql.jdbc.Statement;
 import entidades.*;
 
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import conexaoDB.*;
 import java.util.Locale;
 import java.util.Scanner;
 
 class Main {
 
   public static void main(String[] args) {
-    System.out.println("Hello world!");
     Locale.setDefault(Locale.US);
     Scanner sc = new Scanner(System.in);
 
-    try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/clinica-veterinaria", "root", "")) {
-      System.out.println("Database connected!");
-  } catch (SQLException e) {
-      throw new IllegalStateException("Cannot connect the database!", e);
-  }
+    ConexaoDB mysql = new ConexaoDB();
+
+    try {
+      mysql.conectar();
+
+      var resultado = mysql.getCliente();
+      System.out.println(resultado.getEmailCliente());
+
+    } catch (Exception e) {
+      System.out.println(e);
+    }
   }
 }
