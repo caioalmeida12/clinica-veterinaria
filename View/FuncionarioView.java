26c2c6939f34e5c6a6ffd80124c6af035b660148
Luan <luaninfo09@gmail.com>
2022-10-20 09:10:05 -0300

    Criado o View de Produto,Cliente,Animal,Servico e Funcionario

---
+++ View/FuncionarioView.java
@@ -0,0 +1,14 @@
+package View;
+
+import javax.swing.*;
+
+public class FuncionarioView {
+    private JButton cadastrarFuncionarioButton;
+    private JTextField txtNome;
+    private JTextField txtCPF;
+    private JTextField txtNascimento;
+    private JTextField txtEmail;
+    private JTextField txtSalario;
+    private JTextField txtTelefone;
+    private JTextField txtEndereco;
+}
