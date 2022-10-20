5c4493a2ab47f4f0d075056a4cf89b128209588f
Luan <luaninfo09@gmail.com>
2022-10-20 09:01:44 -0300

    Criado o View de Produto,Cliente,Animal e Servico

--- View/ServicoView.java
+++ View/ServicoView.java
@@ -1,4 +1,10 @@
 package View;
 
+import javax.swing.*;
+
 public class ServicoView {
+    private JButton cadastrarServicoButton;
+    private JTextField txtTipo;
+    private JTextField txtDescricao;
+    private JTextField txtPreco;
 }
