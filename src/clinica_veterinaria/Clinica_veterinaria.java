f19492cb966d1d3f3ac75b5bbc201d3be373f0a3
caioalmeida08 <araujo.caio08@aluno.ifce.edu.br>
2022-11-03 15:05:41 -0300

    modularizei a classe AnimalClienteDB, que fazia parte da ConexaoDB

--- src/clinica_veterinaria/Clinica_veterinaria.java
+++ src/clinica_veterinaria/Clinica_veterinaria.java
@@ -1,5 +1,6 @@
 package clinica_veterinaria;
 
+import conexaoDB.AnimalClienteDB;
 import conexaoDB.AnimalDB;
 import conexaoDB.ClienteDB;
 import conexaoDB.ConexaoDB;
@@ -7,6 +8,7 @@ import conexaoDB.FuncionarioDB;
 import conexaoDB.ProdutoDB;
 import conexaoDB.ServicoDB;
 import conexaoDB.VendaDB;
+import entidades.AnimalCliente;
 import entidades.Produto;
 
 public class Clinica_veterinaria {
@@ -14,9 +16,9 @@ public class Clinica_veterinaria {
     public static void main(String[] args) {
         // new ProdutoView().setVisible(true);
         try {
-            var mysql = new VendaDB();
-            var resultado = mysql.selectVenda().get(0);
-            System.out.println(resultado.getIdProduto());
+            var mysql = new AnimalClienteDB();
+            var resultado = mysql.selectAnimalCliente().get(0);
+            System.out.println(resultado.getIdAnimal());
         } catch (Exception e) {
         }
     }
