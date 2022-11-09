25c0f34b826db67b28b9ca20f582a3814f40e64e
Luan <luaninfo09@gmail.com>
2022-11-09 15:49:06 -0300

    Adicionado a funcao de carregar valores nos campos pelo view do Animal

--- src/View/AnimalView.java
+++ src/View/AnimalView.java
@@ -6,8 +6,10 @@ package View;
 
 import conexaoDB.AnimalDB;
 import conexaoDB.FuncionarioDB;
+import conexaoDB.ProdutoDB;
 import entidades.Animal;
 import entidades.Funcionario;
+import entidades.Produto;
 import java.sql.Date;
 import java.util.ArrayList;
 import javax.swing.table.DefaultTableModel;
@@ -53,6 +55,9 @@ public class AnimalView extends javax.swing.JFrame {
         cadastrarAnimal = new javax.swing.JButton();
         jScrollPane2 = new javax.swing.JScrollPane();
         tabelaAnimal = new javax.swing.JTable();
+        alterarAnimal = new javax.swing.JButton();
+        jLabel7 = new javax.swing.JLabel();
+        txtId = new javax.swing.JTextField();
 
         jTable1.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
@@ -114,7 +119,21 @@ public class AnimalView extends javax.swing.JFrame {
                 return canEdit [columnIndex];
             }
         });
+        tabelaAnimal.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                tabelaAnimalMouseClicked(evt);
+            }
+        });
         jScrollPane2.setViewportView(tabelaAnimal);
+
+        alterarAnimal.setText("Alterar");
+        alterarAnimal.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                alterarAnimalActionPerformed(evt);
+            }
+        });
+
+        jLabel7.setText("Id");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -133,7 +152,15 @@ public class AnimalView extends javax.swing.JFrame {
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                                     .addComponent(txtSexo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                                     .addComponent(txtEspecie, javax.swing.GroupLayout.PREFERRED_SIZE, 126, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jLabel6)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(txtNascimento, javax.swing.GroupLayout.PREFERRED_SIZE, 110, javax.swing.GroupLayout.PREFERRED_SIZE))
                             .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                                .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+                                    .addComponent(jLabel7)
+                                    .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                    .addComponent(txtId))
                                 .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
                                     .addComponent(jLabel3)
                                     .addGap(18, 18, 18)
@@ -145,14 +172,12 @@ public class AnimalView extends javax.swing.JFrame {
                                 .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
                                     .addComponent(jLabel1)
                                     .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                    .addComponent(txtNome, javax.swing.GroupLayout.PREFERRED_SIZE, 115, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                            .addGroup(layout.createSequentialGroup()
-                                .addComponent(jLabel6)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(txtNascimento, javax.swing.GroupLayout.PREFERRED_SIZE, 110, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                                    .addComponent(txtNome, javax.swing.GroupLayout.PREFERRED_SIZE, 115, javax.swing.GroupLayout.PREFERRED_SIZE)))))
                     .addGroup(layout.createSequentialGroup()
                         .addContainerGap()
-                        .addComponent(cadastrarAnimal))
+                        .addComponent(cadastrarAnimal)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(alterarAnimal))
                     .addGroup(layout.createSequentialGroup()
                         .addContainerGap()
                         .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 476, javax.swing.GroupLayout.PREFERRED_SIZE)))
@@ -161,7 +186,10 @@ public class AnimalView extends javax.swing.JFrame {
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel7)
+                    .addComponent(txtId, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel1)
                     .addComponent(txtNome, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
@@ -186,14 +214,29 @@ public class AnimalView extends javax.swing.JFrame {
                     .addComponent(jLabel6)
                     .addComponent(txtNascimento, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
-                .addComponent(cadastrarAnimal)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(cadastrarAnimal)
+                    .addComponent(alterarAnimal))
                 .addGap(18, 18, 18)
                 .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 275, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(30, Short.MAX_VALUE))
+                .addContainerGap(8, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void alterarAnimalActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_alterarAnimalActionPerformed
+        // TODO add your handling code here:
+        
+        AlterarAnimal();
+        listarValores();
+    }//GEN-LAST:event_alterarAnimalActionPerformed
+
+    private void tabelaAnimalMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_tabelaAnimalMouseClicked
+        // TODO add your handling code here:
+        
+        CarregarProduto();
+    }//GEN-LAST:event_tabelaAnimalMouseClicked
 
     private void txtSexoActionPerformed(java.awt.event.ActionEvent evt) {// GEN-FIRST:event_txtSexoActionPerformed
         // TODO add your handling code here:
@@ -250,6 +293,7 @@ public class AnimalView extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton alterarAnimal;
     private javax.swing.JButton cadastrarAnimal;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
@@ -257,12 +301,14 @@ public class AnimalView extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JTable jTable1;
     private javax.swing.JTable tabelaAnimal;
     private javax.swing.JTextField txtCor;
     private javax.swing.JTextField txtEspecie;
+    private javax.swing.JTextField txtId;
     private javax.swing.JTextField txtNascimento;
     private javax.swing.JTextField txtNome;
     private javax.swing.JTextField txtRaca;
@@ -313,16 +359,57 @@ public void cadastrarAnimal(){
         String cor = txtCor.getText();
         Date data = Date.valueOf(txtNascimento.getText());
 
-        entidades.Animal an = new entidades.Animal(nome, raca, data, especie, sexo, cor);
+        Animal animal = new Animal(nome, raca, data, especie, sexo, cor);
 
         AnimalDB mysql = new AnimalDB();
 
         try {
-            mysql.insertAnimal(an);
+            mysql.insertAnimal(animal);
         } catch (Exception ex) {
             System.out.println("erro no view para inserir animal");
             throw new RuntimeException(ex);
         }
 }
+
+ private void AlterarAnimal() {
+
+        
+
+       int id = Integer.parseInt(txtId.getText());
+     String nome = txtNome.getText();
+        String raca = txtRaca.getText();
+        String especie = txtEspecie.getText();
+        String sexo = (String) txtSexo.getSelectedItem();
+        String cor = txtCor.getText();
+        Date data = Date.valueOf(txtNascimento.getText());
+        
+        Animal animal = new Animal(id, nome, raca, data, especie, sexo, cor);
+        
+        AnimalDB mysql = new AnimalDB();
+
+        
+        try {
+            mysql.updateAnimal(animal);
+        } catch (Exception ex) {
+            System.out.println("erro no view para alterar animal");
+            throw new RuntimeException(ex);
+        }
+
+    }
+ 
+ 
+  private void CarregarProduto() {
+
+        int setar = tabelaAnimal.getSelectedRow();
+
+        txtId.setText(tabelaAnimal.getModel().getValueAt(setar, 0).toString());
+        txtNome.setText(tabelaAnimal.getModel().getValueAt(setar, 1).toString());
+        txtRaca.setText(tabelaAnimal.getModel().getValueAt(setar, 2).toString());
+        txtCor.setText(tabelaAnimal.getModel().getValueAt(setar, 3).toString());
+        txtSexo.setSelectedItem(tabelaAnimal.getModel().getValueAt(setar, 4).toString());
+        txtEspecie.setText(tabelaAnimal.getModel().getValueAt(setar, 5).toString());
+        txtNascimento.setText(tabelaAnimal.getModel().getValueAt(setar, 6).toString());
+
+    }
 
 }
